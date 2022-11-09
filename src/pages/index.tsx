747fb26dcd052985a4cd394390e3d0b4482c3f2f
mizuid <117636273+mizuid@users.noreply.github.com>
2022-11-09 15:15:37 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,14 @@ const RenderPage = () => (
     <DocumentHead />
 
     <div>
-      <h2>Welcome!</h2>
-      <p>Your easy-notion-blog deployed successfully!</p>
-      <p>Have fun!</p>
+      <h2>真っ赤な空を見ただろうか！</h2>
+      <p>理屈ばかりこねまわして　すっかり冷めた胸の奥が</p>
+      <p>ただ一度の微笑みで</p>
+      <p>こんなに見事に燃えるとは</p>
       <p>
-        easy-notion-blog powered by{' '}
-        <ExtLink href="https://github.com/otoyo/easy-notion-blog">
-          otoyo/easy-notion-blog
+        kaminarigumo-official-blog powered by{' '}
+        <ExtLink href="http://www.kaminarigumo.com/blog/">
+          kaminarigumo/official-blog
         </ExtLink>
       </p>
     </div>
